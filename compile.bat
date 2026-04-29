e1e9afa9a3a763816993a356850c5f4e2228ec20
rishivengala11 <vengalarishi143@gmail.com>
2026-04-29 12:23:24 +0530

    Compiled with Java 17 for compatibility

--- compile.bat
+++ compile.bat
@@ -5,12 +5,11 @@ set CLASSPATH="C:\xampp\tomcat\lib\servlet-api.jar";"WEB-INF\classes";"WEB-INF\l
 
 if not exist "WEB-INF\classes" mkdir "WEB-INF\classes"
 
-javac --release 21 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\util\DBConnection.java
-javac --release 21 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\model\LostItem.java
-javac --release 21 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\model\FoundItem.java
-javac --release 21 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\dao\LostItemDAO.java
-javac --release 21 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\dao\FoundItemDAO.java
-javac --release 21 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\servlet\*.java
-
+javac --release 17 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\util\DBConnection.java
+javac --release 17 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\model\LostItem.java
+javac --release 17 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\model\FoundItem.java
+javac --release 17 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\dao\LostItemDAO.java
+javac --release 17 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\dao\FoundItemDAO.java
+javac --release 17 -d "WEB-INF\classes" -cp %CLASSPATH% WEB-INF\classes\com\lostandfound\servlet\*.java
 echo Compilation complete.
 pause
